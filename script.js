a205637f760b5f0ecac7ca6866b9b135cdc8a269
Sitti Rahma <rahmayh@Fairtechs-Mac-mini-3.local>
2023-07-26 10:24:35 +0800

    my resume

---
+++ script.js
@@ -0,0 +1,68 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+$(function() {
+    $('#menu > ul').dropotron({
+        mode: 'fade',
+        globalOffsetY: 11,
+        offsetY: -15
+    });
+    $('#slider').slidertron({
+        viewerSelector: '.viewer',
+        indicatorSelector: '.indicator span',
+        reelSelector: '.reel',
+        slidesSelector: '.slide',
+        speed: 'slow',
+        advanceDelay: 4000
+    });
+});
+
+// AJAX Implementation
+document.getElementById("ajaxButton").addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                // Tampilkan respons JSON di bawah tombol
+                document.getElementById("resultText").innerText = JSON.stringify(response, null, 2);
+            } else {
+                document.getElementById("resultText").innerText = "Gagal mengambil data dari API Reddit.";
+            }
+        }
+    };
+    xhr.open("GET", "https://www.gov.uk/bank-holidays.json", true);
+    xhr.send();
+});
+
+// Education Slider
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".slider");
+    const slides = document.querySelectorAll(".slide");
+    const indicators = document.querySelectorAll(".indicator");
+    const slideWidth = slides[0].clientWidth;
+    let currentSlide = 0;
+
+    function goToSlide(slideIndex) {
+        currentSlide = slideIndex;
+        updateSlider();
+    }
+
+    function updateSlider() {
+        slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+        indicators.forEach((indicator, index) => {
+            indicator.classList.toggle("active", index === currentSlide);
+        });
+    }
+
+    indicators.forEach((indicator, index) => {
+        indicator.addEventListener("click", () => goToSlide(index));
+    });
+});
+
+
+// side-bar
